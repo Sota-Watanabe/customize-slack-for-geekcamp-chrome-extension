3ea2f42450d925770c2013b2ff03d030779df138
Sota-Watanabe <stronger.riceball@gmail.com>
2023-10-05 18:31:37 +0900

    ✨feature: トップに文言追加（別方法）

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,7 +1,14 @@
-import { printLine } from './modules/print';
-
-setTimeout(() => {
-  const topSpaceNode = document.querySelector('.p-ia4_top_nav__left_container');
-  printLine('topSpaceNode', topSpaceNode);
-  topSpaceNode.innerHTML = `<p>拡張機能 適応中</p>`;
-}, 3000);
+// トップに文言追加
+window.addEventListener('load', setTextInTop, false);
+function setTextInTop() {
+  const jsInitCheckTimer = setInterval(jsLoaded, 1000);
+  function jsLoaded() {
+    const topSpaceNode = document.querySelector(
+      '.p-ia4_top_nav__left_container'
+    );
+    if (topSpaceNode) {
+      clearInterval(jsInitCheckTimer);
+      topSpaceNode.innerHTML = `<p>拡張機能 適応中</p>`;
+    }
+  }
+}
