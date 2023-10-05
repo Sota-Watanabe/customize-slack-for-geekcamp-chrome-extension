354497ffeb6bb5b58dc541e725f3a40ddd71919e
Sota-Watanabe <stronger.riceball@gmail.com>
2023-10-05 18:15:49 +0900

    ✨feature: トップに文言追加（動かない）

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -4,3 +4,6 @@ console.log('Content script works!');
 console.log('Must reload extension for modifications to take effect.');
 
 printLine("Using the 'printLine' function from the Print Module");
+
+const topSpaceNode = document.querySelector('.p-ia4_top_nav__left_container');
+topSpaceNode.innerHTML = `<p>拡張機能 適応中</p>`;
