4396ff9b05042778d510cd34998c12f8900f2df4
Sota-Watanabe <stronger.riceball@gmail.com>
2023-10-16 19:18:19 +0900

    ✨feature: 矢印追加

--- src/pages/Content/modules/set-close-side-panel-bottom.js
+++ src/pages/Content/modules/set-close-side-panel-bottom.js
@@ -2,14 +2,16 @@ export const setCloseSidePanelBottom = () => {
   const sidebarNode = document.querySelector(
     '.p-ia4_client__resizer--secondary'
   );
-  const divElem = document.createElement('div');
-  divElem.style.position = 'absolute';
-  divElem.style.background = 'red';
-  divElem.style.width = '100px';
-  divElem.style.height = '100px';
-  divElem.style.top = '50%';
-  divElem.style.transform = 'translate(-100%, -50%)';
-  divElem.style.cursor = 'pointer';
-  divElem.innerText = 'ボタン';
-  sidebarNode.appendChild(divElem);
+  const image = document.createElement('img');
+
+  image.style.position = 'absolute';
+  image.style.width = '100px';
+  image.style.height = '100px';
+  image.style.top = '50%';
+  image.style.transform = 'translate(-100%, -50%)';
+  image.style.cursor = 'pointer';
+  image.src = chrome.runtime.getURL('arrow.png');
+  image.alt = 'アイコン';
+
+  sidebarNode.appendChild(image);
 };
